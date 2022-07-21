3c65080b9e519eefb4e0a00558a6f276f75e5b13
Derek Vogt <derekvogt3@gmail.com>
2022-07-20 20:03:50 -0400

    commit tree 11b9f477f8f19788b728ad883f84dbf5170ad3bb

---
+++ lib/song.rb
@@ -0,0 +1,52 @@
+class Song
+
+    @@count = 0
+    @@artists = []
+    @@genres = []
+
+    attr_accessor :name, :artist, :genre
+
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@artists = @@artists + [artist]
+        @@genres = @@genres + [genre]
+    end
+
+    def name
+        @name
+    end
+
+    def artist
+        @artist
+    end
+    def genre
+        @genre
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.genre_count
+        @@genres.tally
+    end
+
+    def self.artist_count
+        @@artists.tally
+    end
+
+
+
+end
